c5480c7a59029335296145489725521e86d5bf13
chedli01 <chedli.masmoudi01@gmail.com>
2025-04-22 13:53:37 +0100

    first schema + resolver to test

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+
+import { createServer } from 'node:http'
+import { createYoga, createSchema } from 'graphql-yoga'
+import { readFileSync } from 'node:fs'
+import { fileURLToPath } from 'node:url'
+import { dirname, join } from 'node:path'
+import { Query } from './resolvers/Query.js'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+const typeDefs = readFileSync(join(__dirname, '/schema/schema.graphql'), 'utf-8')
+
+
+const resolvers = {
+  Query
+};
+
+const yoga = createYoga({
+  schema:createSchema({typeDefs,resolvers})});
+
+const server = createServer(yoga);
+
+server.listen(4000, () => {
+  console.log('🚀 Server ready at http://localhost:4000/graphql');
+});
